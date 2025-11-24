37e67c1a4d330cfc4888cc5ff7c2e59fa5700ce0
orangejack <79419803+kingcat47@users.noreply.github.com>
2025-11-25 07:26:15 +0900

    Home에서 section1로 정보를 불러오는데 필요없어서 섹션1없엠

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,13 +1,48 @@
+import { useState } from "react";
 import Header from "@/components/layout/header";
 import MainLayout from "@/components/layout/main";
-import Section1 from "./section1";
+import Video from "@/components/page/home/video";
+import styles from "./styles.module.scss";
 
 export default function Home() {
+  const [remoteStream, _setRemoteStream] = useState<MediaStream | null>(null);
+  const [isVideoOn, setIsVideoOn] = useState(true);
+
+  const handleVideoToggle = () => {
+    setIsVideoOn((prev) => !prev);
+  };
+
+  // TODO: WebRTC 연결 후 remoteStream 설정
+  // 예: _setRemoteStream(stream);
+
   return (
     <>
       <Header />
       <MainLayout>
-        <Section1 />
+        <div className={styles.container}>
+          <div className={styles.video_grid}>
+            {/* 왼쪽: 본인 화면 */}
+            <div className={styles.local_section}>
+              <div className={styles.video_wrapper}>
+                <Video
+                  mode={isVideoOn ? "camera" : "only-voice"}
+                  showControls={true}
+                  onVideoToggle={handleVideoToggle}
+                  isVideoOn={isVideoOn}
+                />
+                <div className={styles.video_label}>나</div>
+              </div>
+            </div>
+
+            {/* 오른쪽: 상대방 화면 */}
+            <div className={styles.remote_section}>
+              <div className={styles.video_wrapper}>
+                <Video mode="default" remoteStream={remoteStream} />
+                <div className={styles.video_label}>상대방</div>
+              </div>
+            </div>
+          </div>
+        </div>
       </MainLayout>
     </>
   );
